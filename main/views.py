be1bc37fb00779086645f51c085705b06280d454
Ali-3746 <alisher_sos@mail.ru>
2022-11-01 17:04:37 +0600

    first_commit

---
+++ main/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Room
+
+# rooms = [{'id': 1, 'name': 3},
+#          {'id': 2, 'name': 3},
+#          {'id': 3, 'name': 3}
+#          ]
+
+def index(request):
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'main/index.html', context)
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+
+    return render(request, 'main/room.html', context)
