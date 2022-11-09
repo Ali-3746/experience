adb17da708c8198a79d418988545f19b6bded270
Ali-3746 <alisher_sos@mail.ru>
2022-11-09 23:11:11 +0600

    update

--- main/views.py
+++ main/views.py
@@ -30,5 +30,10 @@ def createroom(request):
 def updateroom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+        return redirect('index')
     context = {'form': form}
     return render(request, 'main/room_form.html', context)
