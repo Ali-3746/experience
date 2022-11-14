1b5b37be1bd93374adb5fea085acaa73462dedad
Ali-3746 <alisher_sos@mail.ru>
2022-11-15 00:58:36 +0600

    seatch_pparamtry

--- main/views.py
+++ main/views.py
@@ -8,7 +8,8 @@ from .forms import RoomForm
 #          ]
 
 def index(request):
-    rooms = Room.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(topic__name__icontains=q)
     topics = Topic.objects.all()
     context = {'rooms': rooms, 'topics': topics}
     return render(request, 'main/index.html', context)
